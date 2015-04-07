1c92fd352025257d0d56814acc57c2795c74c735
Sergey Morozov <sergey@slack-corp.com>
2015-04-07 16:49:13 -0700

    Added User Agent to go around bot detection

--- slack_flight_tracker.php
+++ slack_flight_tracker.php
@@ -48,7 +48,18 @@
 	$query = str_replace(' ', '+', strtolower($_REQUEST['text']));	// example query "us+airways+2029"
 
 	$url = "https://www.google.com/search?q=$query";
-	$html = file_get_html($url);
+	
+	$opts = array(
+		'http'=>array(
+	    	'method'=>"GET",
+			'header'=>"Accept-language: en-us\r\n" .
+				  "User-Agent: Mozilla/4.0 (compatible; MSIE 8.0; Windows NT 6.1; WOW64; Trident/4.0; SLCC2; .NET CLR 2.0.50727; .NET CLR 3.5.30729; .NET CLR 3.0.30729; Media Center PC 6.0; MS-RTC LM 8; InfoPath.3; .NET4.0C; .NET4.0E) chromeframe/8.0.552.224\r\n"
+		)
+	);
+	
+	$context = stream_context_create($opts);
+	$rsp = file_get_contents($url, false, $context);
+	$html = str_get_html($rsp);
 	
 	// Let's scrape the google page and see if there is a result resembling a flight status. This is probably the most fragile part of the whole thing. If Google
 	// changes their format most of the stuff below will have to be redone.
